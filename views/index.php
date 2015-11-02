e9840728b8e27633377c133c12917510d31f9ecd
Florian <florian@yootheme.com>
2015-11-02 11:58:56 +0100

    cleanup

--- views/index.php
+++ views/index.php
@@ -1,9 +1,9 @@
 <h1>TODO</h1>
-<ul>
 
+<ul>
 <?php foreach($entries as $entry): ?>
     <?php  if(!$entry['done']): ?>
-        <li><?= $entry['message'] ?></li>
+    <li><?= $entry['message'] ?></li>
     <?php endif ?>
 <?php endforeach; ?>
 </ul>
